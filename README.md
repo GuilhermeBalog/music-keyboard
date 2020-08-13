3d03c6217b496a736288b7f9d741c42e2a971152
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2020-08-13 02:17:19 -0300

    attack and release implementation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Music Keyboard
 
-A simple keyboard made in with the [Tone.js](https://github.com/Tonejs/Tone.js?utm_source=recordnotfound.com) framework
+🎹 A simple keyboard made in with the [Tone.js](https://github.com/Tonejs/Tone.js) framework
 
 ![Music Keyboard](screenshot.png)
 
@@ -15,6 +15,6 @@ git clone https://github.com/GuilhermeBalog/minesweeper.git
 - Open the `index.html` file on your favorite browser
 - Play with your keyboard!
 
-> If you want, check the `index.html` file and feel free to fork this repo and add your ideias!
+> If you want, check the `index.html` and `js/main.js` files and feel free to fork this repo and add your ideias!
 
 *That's all folks! Keep coding!*
--- js/main.js
+++ js/main.js
@@ -2,83 +2,75 @@ const synth = new Tone.PolySynth().toMaster();
 
 var altura = 4;
 
-const teclas = document.querySelectorAll(".tecla")
+const notasNasTeclas = {
+    KeyA: 'C',
+    KeyW: 'C#',
+    KeyS: 'D',
+    KeyE: 'D#',
+    KeyD: 'E',
+    KeyF: 'F',
+    KeyT: 'F#',
+    KeyG: 'G',
+    KeyY: 'G#',
+    KeyH: 'A',
+    KeyU: 'A#',
+    KeyJ: 'B',
+}
 
-teclas.forEach(tecla => {
-    tecla.onclick = () => {
-        var nota = tecla.id;
-        synth.triggerAttackRelease(nota + altura, "8n")
+const estaTocando = {
+    'C': false,
+    'C#': false,
+    'D': false,
+    'D#': false,
+    'E': false,
+    'F': false,
+    'F#': false,
+    'G': false,
+    'G#': false,
+    'A': false,
+    'A#': false,
+    'B': false,
+}
+
+document.addEventListener('keydown', event => {
+    event.preventDefault()
+    const nota = notasNasTeclas[event.code]
+    if(nota){
+        apertarTecla(nota)
     }
 })
 
-document.addEventListener('keypress', function (event) {
-
-    event.preventDefault();
-
-    const notas = {
-        KeyA: () => {
-            synth.triggerAttackRelease("C" + altura, "8n");
-            document.getElementById('C').style.background = 'grey';
-            setTimeout(function () { document.getElementById('C').style.background = 'white'; }, 50);
-        },
-        KeyW: () => {
-            synth.triggerAttackRelease("C#" + altura, "8n");
-            document.getElementById('C#').style.background = 'black';
-            setTimeout(function () { document.getElementById('C#').style.background = '#333'; }, 50);
-        },
-        KeyS: () => {
-            synth.triggerAttackRelease("D" + altura, "8n");
-            document.getElementById('D').style.background = 'grey';
-            setTimeout(function () { document.getElementById('D').style.background = 'white'; }, 50);
-        },
-        KeyE: () => {
-            synth.triggerAttackRelease("D#" + altura, "8n");
-            document.getElementById('D#').style.background = 'black';
-            setTimeout(function () { document.getElementById('D#').style.background = '#333'; }, 50);
-        },
-        KeyD: () => {
-            synth.triggerAttackRelease("E" + altura, "8n");
-            document.getElementById('E').style.background = 'grey';
-            setTimeout(function () { document.getElementById('E').style.background = 'white'; }, 50);
-        },
-        KeyF: () => {
-            synth.triggerAttackRelease("F" + altura, "8n");
-            document.getElementById('F').style.background = 'grey';
-            setTimeout(function () { document.getElementById('F').style.background = 'white'; }, 50);
-        },
-        KeyT: () => {
-            synth.triggerAttackRelease("F#" + altura, "8n");
-            document.getElementById('F#').style.background = 'black';
-            setTimeout(function () { document.getElementById('F#').style.background = '#333'; }, 50);
-        },
-        KeyG: () => {
-            synth.triggerAttackRelease("G" + altura, "8n");
-            document.getElementById('G').style.background = 'grey';
-            setTimeout(function () { document.getElementById('G').style.background = 'white'; }, 1000);
-        },
-        KeyY: () => {
-            synth.triggerAttackRelease("G#" + altura, "8n");
-            document.getElementById('G#').style.background = 'black';
-            setTimeout(function () { document.getElementById('G#').style.background = '#333'; }, 1000);
-        },
-        KeyH: () => {
-            synth.triggerAttackRelease("A" + altura, "8n");
-            document.getElementById('A').style.background = 'grey';
-            setTimeout(function () { document.getElementById('A').style.background = 'white'; }, 1000);
-        },
-        KeyU: () => {
-            synth.triggerAttackRelease("A#" + altura, "8n");
-            document.getElementById('A#').style.background = 'black';
-            setTimeout(function () { document.getElementById('A#').style.background = '#333'; }, 1000);
-        },
-        KeyJ: () => {
-            synth.triggerAttackRelease("B" + altura, "8n");
-            document.getElementById('B').style.background = 'grey';
-            setTimeout(function () { document.getElementById('B').style.background = 'white'; }, 1000);
-        },
+document.addEventListener('keyup', event => {
+    event.preventDefault()
+    const nota = notasNasTeclas[event.code]
+    if(nota){
+        soltarTecla(nota)
     }
-
-    const noteToPlay = notas[event.code] || function(){}
-    noteToPlay()
-
 })
+
+document.addEventListener('mousedown', event => { 
+    event.preventDefault()
+    const nota = event.target.id
+    apertarTecla(nota) 
+})
+
+document.addEventListener('mouseup', event => { 
+    event.preventDefault()
+    const nota = event.target.id
+    soltarTecla(nota) 
+})
+
+function apertarTecla(nota){
+    if(!estaTocando[nota]){
+        synth.triggerAttack(nota + altura)
+        estaTocando[nota] = true
+        document.getElementById(nota).classList.add('active');
+    }
+}
+
+function soltarTecla(nota){
+    synth.triggerRelease(nota + altura)
+    estaTocando[nota] = false
+    document.getElementById(nota).classList.remove('active');
+}
+
